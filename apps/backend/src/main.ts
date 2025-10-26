a8e0e1106d4e7e0838ae69cff6a4ca141462af0c
Alejandrogv2304 <alejandrogv.1011@gmail.com>
2025-10-26 01:28:50 -0500

    Ajustando para despliegue

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,7 +6,12 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://task-manager-0d77.onrender.com'],
+    origin: [
+      'http://localhost:5173',
+      'https://task-manager-0d77.onrender.com',
+      'http://18.191.179.122:5173',
+      'http://18.191.179.122:4000',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
